ea29f51f47511f9484b194bdd0273581b92f8df2
TheReDNooB <xmiguelx209@gmail.com>
2024-11-18 17:14:41 -0500

    rutas con decoradores

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Home'
+
+@app.route('/about')
+def about():
+    return 'About'
+
+if __name__ == '__main__':
+    app.run(debug=True)
