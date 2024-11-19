28c470b42075893e1a12252f03cbe1fc54d773fe
TheReDNooB <xmiguelx209@gmail.com>
2024-11-19 13:57:41 -0500

    construyendo un crud, implementando SQLAchemy y Scss

--- index.py
+++ index.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
